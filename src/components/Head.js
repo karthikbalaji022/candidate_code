01866ed16c113d1416f0908f4fdabd4869d45bcd
karthikbalaji022 <karthikbalaji022@gmail.com>
2022-04-04 11:07:18 +0530

    line count reduced for head comp

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,32 +6,26 @@ import "./head.css";
 import {AppContext} from '../App'
 function Head() {
     const {state,dispatch}=useContext(AppContext);
-    const changeValue=(e)=>{
-        dispatch({type:'SearchChange',data:e})
-    };
   return (
-    <div className="headContainer">
-      <div className="left">
+    <div style={{width:"100%",display: "flex",justifyContent: "space-between",height:"70px"}}>
+      <div className="left" style={{display:"flex",justifyContent: "center",alignItems:"center",gap:"20px",padding: "20px"}}>
         <div className="accountImageContainer">
           <div className="accountImagedesign"></div>
-          <img src={account} className="accountImage" />
+          <img src={account} style={{ width:"20px",zIndex: 2,filter: "brightness(0) invert(1)",borderRadius: "100%"}} />
         </div>
-        <h1>iamneo.ai Talent Center</h1>
+        <h1 style={{fontSize:"1.5rem",fontWeight: "400",color: "rgb(105, 103, 103)"}}>iamneo.ai Talent Center</h1>
       </div>
 
-      <div className="right">
-        <div className="search">
-          <img src={search} className="searchIcon" />
+      <div style={{  display: "flex",flex: "4",justifyContent: "flex-end",alignItems: "center"}}>
+        <div style={{display: "flex",borderBottom: "1px solid rgba(192, 171, 171, 0.5)",gap:"10px"}}>
+          <img src={search} style={{width:"20px",height: "20px"}} />
           <input
-            className="searchContainer"
-            type="text"
-            id="searchBar"
-            placeholder="Search"
-            onChange={e=>changeValue(e.target.value)}
+            style={{ outline: 0,border: 0,width:"200px"}} type="text" id="searchBar" placeholder="Search"
+            onChange={e=>dispatch({type:'SearchChange',data:e.target.value})}
           />
         </div>
         <input type="button" value={"+Add New"} className="addNew" />
-        <img src={gifts} className="gifts" />
+        <img src={gifts} style={{width:"50px",height:"40px",marginLeft: "40px",padding:"3px"}} />
         <div className="userLogin">S</div>
       </div>
     </div>
