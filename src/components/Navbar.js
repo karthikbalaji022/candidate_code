481b46b0a5c8b8ba9b455a32bbf3dc1b0bad1955
karthikbalaji022 <karthikbalaji022@gmail.com>
2022-04-04 10:50:30 +0530

    reduced line count for nav sction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ import inbox from '../images/navicon/inbox.png'
 import message from '../images/navicon/message.png'
 import meter from '../images/navicon/meter.png'
 import setting from '../images/navicon/setting.png'
-import threedots from '../images/navicon/threedots.png'
 import users from '../images/navicon/users.png'
 import briefcase from '../images/navicon/briefcase.png'
 import './Nav.css'
@@ -14,25 +13,18 @@ import './Nav.css'
 function Nav(){
 
     return(
-        <div className="Nav">
-            <div className='top'>
-                <div className='userIcon'>
+        <div className="Nav" style={{width:"50px",height:"100vh",padding:"10px",display: "flex",flexDirection: "column",justifyContent: "space-between"}}>
+            <div style={{display: "flex",flexDirection: "column",alignItems: "center",flex:4,gap:"17px"}}>
+                <div style={{ borderBottom: "1px solid rgba(196, 184, 184, .3)"}}>
                 <img src={account} alt="navicon" className='navIcon'/>
                 </div>
                 <img src={meter} alt="navicon" className='navIcon'/>
                 <img src={inbox} alt="navicon" className='navIcon'/>
-                <div className='dots'>
                 <img src={briefcase} alt="navicon" className='navIcon'/>
-                <img src={threedots} alt="navicon" className='threedot'/>
-                </div>                
-                <div className='dots'>                
                 <img src={users} alt="navicon" className='navIcon'/>
-                <img src={threedots} alt="navicon"  className='threedot'/>
-
-                </div>                
                 <img src={setting} alt="navicon" className='navIcon'/>
             </div>
-            <div className='bottom'>
+            <div style={{display: "flex",flexDirection: "column",alignItems: "center",flex:1}}>
             <img src={help} alt="navicon" className='navIcon'/>
                 <img src={message} alt="navicon" className='navIcon'/>
                 <img src={app_draw} alt="navicon" className='navIcon'/>
