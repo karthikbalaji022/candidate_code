4df670fc8b534756353a373ebbb45c9036b57f0c
karthikbalaji022 <karthikbalaji022@gmail.com>
2022-04-05 19:29:31 +0530

    Ui head resolved

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,6 +2,7 @@ import React,{useContext} from "react";
 import gifts from "../images/head/gifts.png";
 import account from "../images/head/account.png";
 import search from "../images/head/search.png";
+import './head.css'
 import {AppContext} from '../App'
 function Head() {
     const {state,dispatch}=useContext(AppContext);
